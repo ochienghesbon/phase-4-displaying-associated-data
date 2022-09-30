a5308ae5cd58f8846a4a3a9f51dcb3a37346b6be
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-10-01 00:24:26 +0300

    Done.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,11 @@ class ReviewsController < ApplicationController
 
   def index
     reviews = Review.all.order(rating: :desc)
-    render json: reviews
+    render json: reviews, include: :dog_house
+  end
+  def show
+    dog_house = DogHouse.find(params[:id])
+    render json: dog_house, include: :reviews
   end
 
 end
